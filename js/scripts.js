eb16a6cfadfcd71f7321f3e08b25e70dc0baf82b
m-3r <m-3r@outlook.com>
2023-10-08 13:36:45 +0600

    added new feature pop up

--- js/scripts.js
+++ js/scripts.js
@@ -427,3 +427,14 @@
 	});
 
 })(jQuery);
+const popup = document.querySelector('.popup');
+        const x = document.querySelector('.popup-content img')
+
+        window.addEventListener('load', () => {
+            popup.classList.add('showPopup');
+            popup.childNodes[1].classList.add('showPopup');
+        })
+        x.addEventListener('click', () => {
+            popup.classList.remove('showPopup');
+            popup.childNodes[1].classList.remove('showPopup');
+        })
